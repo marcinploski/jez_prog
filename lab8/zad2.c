7efbbf510d219ddaf5486cc32a0fea9059902a67
marcinploski <malyrcin@gmail.com>
2016-05-06 10:04:21 +0200

    zalegle lab

---
+++ lab8/zad2.c
@@ -0,0 +1,5 @@
+#define SZCZYT(kraj, szczyt) Najwy�szym szczytem kraj jest szczyt.
+SZCZYT(Azja, Mt. Everest)
+SZCZYT(Europa, Elbrus)
+SZCZYT(Afryka, Kilimandzaro)
+SZCZYT(Australia, Gora Kosciuszki)
